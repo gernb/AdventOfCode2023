0e6db44e1a271efaae8798a75c56c538bd72a520
peter bohac <gern@1dot0.net>
2023-12-03 21:20:03 -0800

    Day 4, part 1

--- Day 04/Solution.swift
+++ Day 04/Solution.swift
@@ -5,13 +5,40 @@
 //  Copyright © 2023 peter bohac. All rights reserved.
 //
 
+import Foundation
+
 // MARK: - Part 1
+
+struct ScratchCard {
+    let id: Int
+    let winningNumbers: Set<Int>
+    let numbers: [Int]
+}
+extension ScratchCard {
+    init(_ string: String) {
+        var parts = string.split(separator: ":")
+        let id = Int(String(parts[0].split(separator: " ")[1]))!
+        parts = parts[1].split(separator: "|")
+        let winningNumbers = parts[0].trimmingCharacters(in: .whitespaces)
+            .split(separator: " ")
+            .map { Int(String($0))! }
+        let numbers = parts[1].trimmingCharacters(in: .whitespaces)
+            .split(separator: " ")
+            .map { Int(String($0))! }
+        self.init(id: id, winningNumbers: Set(winningNumbers), numbers: numbers)
+    }
+}
 
 enum Part1 {
     static func run(_ source: InputData) {
-        let input = source.data
+        let cards = source.data.map(ScratchCard.init)
+        let matches = cards.map { card in
+            card.numbers.filter { card.winningNumbers.contains($0) }
+        }
+        let total = matches.map { $0.count > 0 ? pow(2, ($0.count - 1)) : 0 }
+            .reduce(0, +)
 
-        print("Part 1 (\(source)):")
+        print("Part 1 (\(source)): \(total)")
     }
 }
 
