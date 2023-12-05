189899cbd7e854af1c7ea2380f10edf7882543b8
peter bohac <gern@1dot0.net>
2023-12-04 23:06:29 -0800

    Day 5, part 2 (slow)

--- Day 05/Solution.swift
+++ Day 05/Solution.swift
@@ -7,16 +7,27 @@
 
 // MARK: - Part 1
 
+enum Category: String {
+    case seed, soil, fertilizer, water, light, temperature, humidity, location
+}
+
 struct Almanac {
     struct Map {
-        let source: String
-        let destination: String
+        let source: Category
+        let destination: Category
         let sourceRanges: [ClosedRange<Int>]
         let destinationRanges: [Int]
 
+        let lowerBound: Int
+        let upperBound: Int
+
         func output(for input: Int) -> Int {
+            guard input >= lowerBound && input <= upperBound else {
+                return input
+            }
             for (idx, range) in sourceRanges.enumerated() {
-                if range.contains(input) {
+//                if range.contains(input) {
+                if input >= range.lowerBound && input <= range.upperBound {
                     return input - range.lowerBound + destinationRanges[idx]
                 }
             }
@@ -24,12 +35,12 @@ struct Almanac {
         }
     }
 
-    let maps: [String: Map]
+    let maps: [Category: Map]
 
     func location(for seed: Int) -> Int {
-        var source = "seed"
+        var source = Category.seed
         var number = seed
-        while source != "location" {
+        while source != .location {
             let map = maps[source]!
             number = map.output(for: number)
             source = map.destination
@@ -46,7 +57,14 @@ extension Almanac.Map {
             let values = line.split(separator: " ").map { Int(String($0))! }
             return (values[1] ... values[1] + values[2] - 1, values[0])
         }
-        self.init(source: source, destination: destination, sourceRanges: ranges.map(\.0), destinationRanges: ranges.map(\.1))
+        self.init(
+            source: Category(rawValue: source)!,
+            destination: Category(rawValue: destination)!,
+            sourceRanges: ranges.map(\.0),
+            destinationRanges: ranges.map(\.1),
+            lowerBound: ranges.map(\.0.lowerBound).min()!,
+            upperBound: ranges.map(\.0.upperBound).max()!
+        )
     }
 }
 extension Almanac {
@@ -73,8 +91,19 @@ enum Part1 {
 
 enum Part2 {
     static func run(_ source: InputData) {
-        let input = source.data
+        let input = source.data.split(separator: "")
+        let seedPairs = input.first!.first!.split(separator: " ").compactMap { Int(String($0)) }
+        let almanac = Almanac(input.dropFirst())
+        var minLocation = Int.max
+        for idx in stride(from: 0, to: seedPairs.count - 1, by: 2) {
+            let seedStart = seedPairs[idx]
+            let count = seedPairs[idx + 1]
+            for seed in seedStart ..< seedStart + count {
+                minLocation = min(minLocation, almanac.location(for: seed))
+            }
+            print(".", terminator: "")
+        }
 
-        print("Part 2 (\(source)):")
+        print("\nPart 2 (\(source)): \(minLocation)")
     }
 }
