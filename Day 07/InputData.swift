c466a4048895cc43eb68bbbc4c7e264fb11081f2
peter bohac <gern@1dot0.net>
2023-12-06 21:35:58 -0800

    Day 7, part 1

--- Day 07/InputData.swift
+++ Day 07/InputData.swift
@@ -16,6 +16,11 @@ enum InputData: String, CaseIterable {
         switch self {
 
         case .example: return """
+32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483
 """.components(separatedBy: .newlines)
 
         case .challenge:
--- Day 07/Solution.swift
+++ Day 07/Solution.swift
@@ -7,11 +7,92 @@
 
 // MARK: - Part 1
 
+enum HandType: Int {
+    case fiveOfAKind = 7
+    case fourOfAKind = 6
+    case fullHouse = 5
+    case threeOfAKind = 4
+    case twoPair = 3
+    case onePair = 2
+    case highCard = 1
+}
+
+struct Hand {
+    let hand: String
+    let handType: HandType
+    let bid: Int
+
+    init(hand: String, bid: Int) {
+        self.hand = hand
+        self.bid = bid
+        self.handType = Self.handType(for: hand)
+    }
+
+    private static func handType(for hand: String) -> HandType {
+        var sets: [Character: Int] = [:]
+        for card in hand {
+            sets[card, default: 0] += 1
+        }
+        if sets.count == 1 {
+            return .fiveOfAKind
+        }
+        if sets.count == 2 {
+            let count = sets.first!.value
+            if count == 4 || count == 1 {
+                return .fourOfAKind
+            } else {
+                return .fullHouse
+            }
+        }
+        if sets.count == 3 {
+            let counts = Set(sets.values)
+            if counts.contains(3) {
+                return . threeOfAKind
+            } else {
+                return .twoPair
+            }
+        }
+        if sets.count == 4 {
+            return .onePair
+        }
+        assert(sets.count == 5)
+        return .highCard
+    }
+}
+extension Hand: Comparable {
+    static let cards = "23456789TJQKA"
+
+    static func < (lhs: Hand, rhs: Hand) -> Bool {
+        guard lhs.handType == rhs.handType else {
+            return lhs.handType.rawValue < rhs.handType.rawValue
+        }
+        for pair in zip(lhs.hand, rhs.hand) {
+            if pair.0 == pair.1 {
+                continue
+            }
+            let lhsIdx = cards.firstIndex(of: pair.0)!
+            let rhsIdx = cards.firstIndex(of: pair.1)!
+            return lhsIdx < rhsIdx
+        }
+        fatalError()
+    }
+}
+extension Hand {
+    init(_ string: String) {
+        let parts = string.split(separator: " ").map(String.init)
+        self.init(hand: parts[0], bid: Int(parts[1])!)
+    }
+}
+
 enum Part1 {
     static func run(_ source: InputData) {
-        let input = source.data
+        let hands = source.data.map(Hand.init)
+        let sortedHands = hands.sorted()
+        let sum = sortedHands.enumerated().reduce(0) { result, pair in
+            result + pair.element.bid * (pair.offset + 1)
+        }
 
-        print("Part 1 (\(source)):")
+        print("Part 1 (\(source)): \(sum)")
     }
 }
 
