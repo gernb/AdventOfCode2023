5e7cae09fe3a29747a01f5349726e290f2018030
peter bohac <gern@1dot0.net>
2023-12-08 21:22:37 -0800

    Day 9, part 1

--- Day 09/Solution.swift
+++ Day 09/Solution.swift
@@ -8,10 +8,35 @@
 // MARK: - Part 1
 
 enum Part1 {
-    static func run(_ source: InputData) {
-        let input = source.data
+    static func nextValue(for history: [Int]) -> Int {
+        var sequences = [history]
+        while sequences.last!.allSatisfy({ $0 == 0 }) == false {
+            let sequence = sequences.last!
+            let newSequence = sequence
+                .enumerated()
+                .dropLast()
+                .map { (idx: Int, value: Int) in
+                    sequence[idx + 1] - value
+                }
+            sequences.append(newSequence)
+        }
+        var nextValue = 0
+        for sequence in sequences.dropLast().reversed() {
+            nextValue += sequence.last!
+        }
+        return nextValue
+    }
 
-        print("Part 1 (\(source)):")
+    static func run(_ source: InputData) {
+        var historyLines = source.data.map { line in
+            line.split(separator: " ").map { Int(String($0))! }
+        }
+        for idx in 0 ..< historyLines.count {
+            historyLines[idx].append(nextValue(for: historyLines[idx]))
+        }
+
+        let sum = historyLines.map { $0.last! }.reduce(0, +)
+        print("Part 1 (\(source)): \(sum)")
     }
 }
 
