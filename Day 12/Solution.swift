4d61e216c85737cb172504b73f01f6167e36444a
peter bohac <gern@1dot0.net>
2023-12-12 00:17:58 -0800

    Day 12, part 1 (speed up) + partial part 2

--- Day 12/Solution.swift
+++ Day 12/Solution.swift
@@ -7,7 +7,6 @@
 
 // MARK: - Part 1
 
-//???.### 1,1,3
 struct Record {
     let condition: String
     let groups: [Int]
@@ -26,22 +25,60 @@ extension Record {
 }
 
 enum Part1 {
-    static func arrangements(for record: Record) -> [String] {
-        var result: [String] = [record.condition]
-        while result[0].contains("?") {
-            let condition = result.removeFirst()
-            result.append(condition.replacing("?", with: ".", maxReplacements: 1))
-            result.append(condition.replacing("?", with: "#", maxReplacements: 1))
+    static func arrangements(_ condition: String, _ groups: [Int]) -> Int {
+        var condition = condition.trimmingCharacters(in: ["."])
+        if condition.count < (groups.reduce(0, +) + groups.count - 1) {
+            return 0
+        }
+        if groups.isEmpty && condition.isEmpty {
+            return 1
+        }
+        if groups.isEmpty && condition.allSatisfy({ $0 == "." || $0 == "?" }) {
+            return 1
+        }
+        if groups.isEmpty || condition.isEmpty {
+            return 0
         }
 
+        var groups = groups
+        var result = 0
+        let char = condition.removeFirst()
+        if char == "?" {
+            result += arrangements(condition, groups)
+        }
+        let hashNeeded = groups.removeFirst()
+        var hashCount = 1
+        while true {
+            while condition.isEmpty == false, condition.first == "#" {
+                condition.removeFirst()
+                hashCount += 1
+            }
+            if hashCount > hashNeeded {
+                return result
+            }
+            if hashCount == hashNeeded {
+                if condition.isEmpty {
+                    return result + arrangements(condition, groups)
+                }
+                condition.removeFirst()
+                return result + arrangements(condition, groups)
+            }
+            if condition.first == "." {
+                return result
+            }
+            if condition.isEmpty {
+                return result
+            }
+            condition.removeFirst()
+            hashCount += 1
+        }
         return result
     }
 
     static func run(_ source: InputData) {
         let records = source.lines.map(Record.init)
-        let counts = records.enumerated().map { (idx: Int, record: Record) in
-            print(idx)
-            return arrangements(for: record).filter(record.matches(_:)).count
+        let counts = records.map { record in
+            arrangements(record.condition, record.groups)
         }
 
         print("Part 1 (\(source)): \(counts.reduce(0, +))")
@@ -51,7 +88,20 @@ enum Part1 {
 // MARK: - Part 2
 
 enum Part2 {
+    static func unfold(_ record: Record) -> Record {
+        let condition = Array(repeating: record.condition, count: 5).joined(separator: "?")
+        let groups = Array(repeating: record.groups, count: 5).flatMap { $0 }
+        return .init(condition: condition, groups: groups)
+    }
+
     static func run(_ source: InputData) {
-        print("Part 2 (\(source)):")
+        let records = source.lines.map(Record.init).map(unfold)
+
+        let counts = records.enumerated().map { (n: Int, record: Record) in
+//            print(n)
+            return Part1.arrangements(record.condition, record.groups)
+        }
+
+        print("Part 2 (\(source)): \(counts.reduce(0, +))")
     }
 }
