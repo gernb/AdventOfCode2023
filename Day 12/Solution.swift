e8941f3c5116baf0bbaade5d09dfbff0a4737f43
peter bohac <gern@1dot0.net>
2023-12-11 21:49:28 -0800

    Day 12, part 1 (slow)

--- Day 12/Solution.swift
+++ Day 12/Solution.swift
@@ -7,9 +7,44 @@
 
 // MARK: - Part 1
 
+//???.### 1,1,3
+struct Record {
+    let condition: String
+    let groups: [Int]
+
+    func matches(_ condition: String) -> Bool {
+        condition.split(separator: ".").map(\.count) == groups
+    }
+}
+extension Record {
+    init(_ line: String) {
+        let parts = line.split(separator: " ").map(String.init)
+        let condition = parts[0]
+        let groups = parts[1].split(separator: ",").map { Int(String($0))! }
+        self.init(condition: condition, groups: groups)
+    }
+}
+
 enum Part1 {
+    static func arrangements(for record: Record) -> [String] {
+        var result: [String] = [record.condition]
+        while result[0].contains("?") {
+            let condition = result.removeFirst()
+            result.append(condition.replacing("?", with: ".", maxReplacements: 1))
+            result.append(condition.replacing("?", with: "#", maxReplacements: 1))
+        }
+
+        return result
+    }
+
     static func run(_ source: InputData) {
-        print("Part 1 (\(source)):")
+        let records = source.lines.map(Record.init)
+        let counts = records.enumerated().map { (idx: Int, record: Record) in
+            print(idx)
+            return arrangements(for: record).filter(record.matches(_:)).count
+        }
+
+        print("Part 1 (\(source)): \(counts.reduce(0, +))")
     }
 }
 
