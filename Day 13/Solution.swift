c002be1e29299fc2ea7394cc1ff18dd2c82a02a4
peter bohac <gern@1dot0.net>
2023-12-12 21:55:43 -0800

    Day 13, part 2

--- Day 13/Solution.swift
+++ Day 13/Solution.swift
@@ -70,7 +70,54 @@ enum Part1 {
 // MARK: - Part 2
 
 enum Part2 {
+    static func diffCount(_ top: [String], _ bottom: [String]) -> Int {
+        zip(top, bottom).reduce(0) { result, pair in
+            result + zip(pair.0, pair.1).reduce(0) { result, pair in
+                result + (pair.0 == pair.1 ? 0 : 1)
+            }
+        }
+    }
+
+    static func summarize(_ pattern: Pattern, skipping value: Int) -> Int {
+        func findLine(_ rows: [String], skipping row: Int?) -> Int? {
+            let count = rows.count
+            var line = 0
+            while line < count - 1 {
+                line += 1
+                if line == row { continue }
+                let remaining = count - line
+                let top: [String]
+                let bottom: [String]
+                if line < remaining {
+                    top = Array(rows.prefix(line))
+                    bottom = Array(rows.dropFirst(line).prefix(line).reversed())
+                } else {
+                    let size = line - remaining
+                    top = Array(rows.dropFirst(size).prefix(remaining))
+                    bottom = Array(rows.dropFirst(line).reversed())
+                }
+                if diffCount(top, bottom) == 1 {
+                    return line
+                }
+            }
+            return nil
+        }
+
+        if let row = findLine(pattern.rows, skipping: value > 100 ? value / 100 : nil) {
+            return row * 100
+        } else {
+            let column = findLine(pattern.rotate(), skipping: value < 100 ? value : nil)!
+            return column
+        }
+    }
+
     static func run(_ source: InputData) {
-        print("Part 2 (\(source)):")
+        let patterns = source.lines.split(separator: "").map(Pattern.init)
+        let original = patterns.map(Part1.summarize(_:))
+        let scores = zip(patterns, original).map { (pattern: Pattern, value: Int) in
+            summarize(pattern, skipping: value)
+        }
+
+        print("Part 2 (\(source)): \(scores.reduce(0, +))")
     }
 }
