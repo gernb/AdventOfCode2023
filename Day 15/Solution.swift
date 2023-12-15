2863aa6f9cdba098ebaedc1df3c393b157435e84
peter bohac <gern@1dot0.net>
2023-12-14 21:50:59 -0800

    Day 15, part 2

--- Day 15/Solution.swift
+++ Day 15/Solution.swift
@@ -24,8 +24,59 @@ enum Part1 {
 
 // MARK: - Part 2
 
+enum Operation: String {
+    case dash = "-"
+    case equals = "="
+}
+
+struct Instruction: Equatable {
+    let label: String
+    let focalLen: Int
+    let operation: Operation
+
+    var hash: Int { Part1.hash(label) }
+
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        lhs.label == rhs.label
+    }
+}
+extension Instruction {
+    init(_ string: any StringProtocol) {
+        if string.hasSuffix("-") {
+            let label = String(string.dropLast())
+            self.init(label: label, focalLen: 0, operation: .dash)
+        } else {
+            let parts = string.split(separator: "=").map { String($0) }
+            let focalLen = Int(parts[1])!
+            self.init(label: parts[0], focalLen: focalLen, operation: .equals)
+        }
+    }
+}
+
 enum Part2 {
     static func run(_ source: InputData) {
-        print("Part 2 (\(source)):")
+        let instructions = source.lines.joined().split(separator: ",").map(Instruction.init)
+        let boxes: [Int: [Instruction]] = instructions.reduce(into: [:]) { result, instruction in
+            var list = result[instruction.hash, default: []]
+            switch instruction.operation {
+            case .dash:
+                list.removeAll(where: { $0 == instruction })
+            case .equals:
+                if let idx = list.firstIndex(of: instruction) {
+                    list[idx] = instruction
+                } else {
+                    list.append(instruction)
+                }
+            }
+            result[instruction.hash] = list
+        }
+        let totalPower = boxes.reduce(0) { result, pair in
+            let box = pair.key
+            return result + pair.value.enumerated().reduce(0, { result, pair in
+                result + (box + 1) * (pair.offset + 1) * pair.element.focalLen
+            })
+        }
+
+        print("Part 2 (\(source)): \(totalPower)")
     }
 }
