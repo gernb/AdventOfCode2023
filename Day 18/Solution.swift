51b7b07b9380932d3edc0ab27db341aeb0e51b4f
peter bohac <gern@1dot0.net>
2023-12-18 00:21:50 -0800

    Day 18, part 2

--- Day 18/Solution.swift
+++ Day 18/Solution.swift
@@ -19,20 +19,24 @@ struct Coordinate: Hashable, CustomStringConvertible {
     var down: Self { .init(x: x, y: y + 1) }
     var left: Self { .init(x: x - 1, y: y) }
     var right: Self { .init(x: x + 1, y: y) }
+    var upLeft: Self { .init(x: x - 1, y: y - 1) }
+    var upRight: Self { .init(x: x + 1, y: y - 1) }
+    var downLeft: Self { .init(x: x - 1, y: y + 1) }
+    var downRight: Self { .init(x: x + 1, y: y + 1) }
 
-    var allDirections: [Self] { [left, up, right, down] }
+    var adjacent: [Self] { [upLeft, up, upRight, left, right, downLeft, down, downRight] }
 }
 
 enum Part1 {
     static func floodFill(from start: Coordinate, initial filled: Set<Coordinate>) -> Set<Coordinate> {
         var filled = filled
-        var queue = [start]
+        var queue: Set<Coordinate> = [start]
         while queue.isEmpty == false {
             let coord = queue.removeFirst()
-            coord.allDirections.forEach { next in
-                if filled.contains(next) == false {
+            coord.adjacent.forEach { next in
+                if filled.contains(next) == false && queue.contains(next) == false {
                     filled.insert(next)
-                    queue.append(next)
+                    queue.insert(next)
                 }
             }
         }
@@ -66,8 +70,64 @@ enum Part1 {
 
 // MARK: - Part 2
 
+struct Edge {
+    let xRange: ClosedRange<Int>
+    let yRange: ClosedRange<Int>
+    var count: Int { xRange.count > 0 ? xRange.count : yRange.count }
+    var isVertical: Bool { yRange.count > 0 }
+    init(_ start: Coordinate, _ end: Coordinate) {
+        self.xRange = min(start.x, end.x) ... max(start.x, end.x)
+        self.yRange = min(start.y, end.y) ... max(start.y, end.y)
+    }
+}
+
 enum Part2 {
     static func run(_ source: InputData) {
-        print("Part 2 (\(source)):")
+        var edges: [Edge] = []
+        var coord = Coordinate.origin
+        for line in source.lines {
+            let parts = line.split(separator: " ")
+            let dirInt = Int(String(parts[2].dropFirst(7).dropLast()))!
+            let count = Int(String(parts[2].dropFirst(2).dropLast(2)), radix: 16)!
+            let end =
+                switch dirInt {
+                case 0: Coordinate(x: coord.x + count, y: coord.y)
+                case 1: Coordinate(x: coord.x, y: coord.y + count)
+                case 2: Coordinate(x: coord.x - count, y: coord.y)
+                case 3: Coordinate(x: coord.x, y: coord.y - count)
+                default: fatalError()
+                }
+            edges.append(Edge(coord, end))
+            coord = end
+        }
+
+        let xValues = edges.flatMap { [$0.xRange.lowerBound, $0.xRange.upperBound] }.sorted()
+        let yValues = edges.flatMap { [$0.yRange.lowerBound, $0.yRange.upperBound] }.sorted()
+        let xRange = xValues.first! ... xValues.last!
+        let yRange = yValues.first! ... yValues.last!
+
+        var rows: [Int: [Int]] = [:]
+        rows.reserveCapacity(yRange.count)
+        for edge in edges {
+            if edge.isVertical {
+                for y in edge.yRange.dropLast() {
+                    rows[y, default: []].append(edge.xRange.lowerBound)
+                }
+            }
+        }
+
+        var fillCount = 0
+        for y in yRange {
+            var columns = rows[y, default: []].sorted()
+            guard columns.count >= 2 else { continue }
+            assert(columns.count.isMultiple(of: 2))
+            for idx in stride(from: 0, to: columns.count - 1, by: 2) {
+                fillCount += columns[idx + 1] - columns[idx] + 1
+            }
+        }
+
+        let perimeter = edges.reduce(0) { $0 + $1.count - 1 }
+
+        print("Part 2 (\(source)): \(fillCount + perimeter / 2 + 1)")
     }
 }
