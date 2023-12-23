6f590f66d4a9fa9de017330c6c7f9e5d3e9db77b
peter bohac <gern@1dot0.net>
2023-12-23 12:25:57 -0800

    Clean-up naming

--- Day 23/Solution.swift
+++ Day 23/Solution.swift
@@ -27,8 +27,6 @@ struct Coordinate: Hashable, CustomStringConvertible {
     var x: Int
     var y: Int
 
-    static let start = Self(x: 1, y: 0)
-
     var description: String { "(\(x), \(y))" }
 
     var up: Self { .init(x: x, y: y - 1) }
@@ -52,10 +50,11 @@ func loadTrailMap(_ lines: [String]) -> [Coordinate: Tile] {
 enum Part1 {
     static func run(_ source: InputData) {
         let map = loadTrailMap(source.lines)
-        let end = Coordinate(x: source.lines[0].count - 2, y: source.lines.count - 1)
+        let trailEntrance = Coordinate(x: 1, y: 0)
+        let trailExit = Coordinate(x: source.lines[0].count - 2, y: source.lines.count - 1)
 
         func findAllPaths(from start: Coordinate, visited: Set<Coordinate> = []) -> [Set<Coordinate>] {
-            if start == end {
+            if start == trailExit {
                 return [visited]
             }
             let next = start.neighbours.filter {
@@ -74,7 +73,7 @@ enum Part1 {
             return next.flatMap { findAllPaths(from: $0, visited: visited) }
         }
 
-        let result = findAllPaths(from: Coordinate.start).map(\.count).max()!
+        let result = findAllPaths(from: trailEntrance).map(\.count).max()!
 
         print("Part 1 (\(source)): \(result)")
     }
@@ -85,56 +84,56 @@ enum Part1 {
 enum Part2 {
     static func run(_ source: InputData) {
         let map = loadTrailMap(source.lines)
-        let end = Coordinate(x: source.lines[0].count - 2, y: source.lines.count - 1)
+        let trailEntrance = Coordinate(x: 1, y: 0)
+        let trailExit = Coordinate(x: source.lines[0].count - 2, y: source.lines.count - 1)
 
-        let splits: [Coordinate] = map.reduce(into: []) { result, pair in
-            if pair.key == Coordinate.start || pair.key == end || pair.value == .forest { return }
-            let next = pair.key.neighbours.filter { map[$0] != .forest }
-            if next.count > 2 {
+        let junctions: [Coordinate] = map.reduce(into: []) { result, pair in
+            guard pair.value != .forest else { return }
+            if pair.key == trailEntrance || pair.key == trailExit {
+                result.append(pair.key)
+            } else if pair.key.neighbours.filter({ map[$0] != .forest }).count > 2 {
                 result.append(pair.key)
             }
         }
-        let nodes = splits + [Coordinate.start, end]
 
-        func findPaths(from start: Coordinate, to nodes: [Coordinate]) -> [(node: Coordinate, distance: Int)] {
-            func findPaths(from start: Coordinate, to nodes: [Coordinate], path: [Coordinate] = []) -> [(node: Coordinate, distance: Int)] {
-                let path = path + [start]
-                if nodes.contains(start) && path.count > 1 {
+        func findLongestDistances(from start: Coordinate, to ends: [Coordinate]) -> [(end: Coordinate, distance: Int)] {
+            func findAllDistances(from start: Coordinate, to ends: [Coordinate], path: Set<Coordinate> = []) -> [(end: Coordinate, distance: Int)] {
+                let path = path.union([start])
+                if ends.contains(start) && path.count > 1 {
                     return [(start, path.count - 1)]
                 }
                 let next = start.neighbours.filter {
                     guard path.contains($0) == false else { return false }
                     return map[$0, default: .forest] != .forest
                 }
-                return next.flatMap { findPaths(from: $0, to: nodes, path: path) }
+                return next.flatMap { findAllDistances(from: $0, to: ends, path: path) }
             }
-            return findPaths(from: start, to: nodes).reduce(into: Dictionary<Coordinate, Int>()) { result, pair in
-                let previous = result[pair.node, default: 0]
-                result[pair.node] = max(previous, pair.distance)
+            return findAllDistances(from: start, to: ends).reduce(into: Dictionary<Coordinate, Int>()) { result, pair in
+                let previous = result[pair.end, default: 0]
+                result[pair.end] = max(previous, pair.distance)
             }
             .map { ($0.key, $0.value) }
         }
 
-        let paths = nodes.reduce(into: [:]) { result, node in
-            result[node] = findPaths(from: node, to: nodes)
+        let trails = junctions.reduce(into: [:]) { result, end in
+            result[end] = findLongestDistances(from: end, to: junctions)
         }
 
-        func dfs(from start: Coordinate, path: [(node: Coordinate, distance: Int)] = []) -> [(node: Coordinate, distance: Int)] {
-            if start == end {
+        func findLongestTrail(from start: Coordinate, path: [(junction: Coordinate, distance: Int)] = []) -> [(junction: Coordinate, distance: Int)] {
+            if start == trailExit {
                 return path
             }
-            let visited = path.map(\.node) + [start]
-            let nextNodes = paths[start]!
-                .filter { visited.contains($0.node) == false }
-                .sorted(by: { $0.distance > $1.distance })
-            return nextNodes.map {
-                let path = dfs(from: $0.node, path: path + [$0])
-                return (path: path, distance: path.reduce(0) { $0 + $1.distance })
+            let visited = Set(path.map(\.junction) + [start])
+            let nextTrails = trails[start]!
+                .filter { visited.contains($0.end) == false }
+            return nextTrails.map {
+                let nextTrail = findLongestTrail(from: $0.end, path: path + [($0.end, $0.distance)])
+                return (trail: nextTrail, distance: nextTrail.reduce(0) { $0 + $1.distance })
             }
-            .max(by: { $0.distance < $1.distance })?.path ?? []
+            .max(by: { $0.distance < $1.distance })?.trail ?? []
         }
 
-        let longestPath = dfs(from: Coordinate.start)
+        let longestPath = findLongestTrail(from: trailEntrance)
 
         print("Part 2 (\(source)): \(longestPath.reduce(0) { $0 + $1.distance })")
     }
