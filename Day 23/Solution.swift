9931438ef4eac429a54d03efd1ebaa2da1a717a8
peter bohac <gern@1dot0.net>
2023-12-23 17:07:41 -0800

    Use generic DFS

--- Day 23/Solution.swift
+++ Day 23/Solution.swift
@@ -37,43 +37,68 @@ struct Coordinate: Hashable, CustomStringConvertible {
     var neighbours: [Self] { [up, left, right, down] }
 }
 
-func loadTrailMap(_ lines: [String]) -> [Coordinate: Tile] {
-    var map: [Coordinate: Tile] = [:]
-    for (y, line) in lines.enumerated() {
-        for (x, char) in line.enumerated() {
-            map[Coordinate(x: x, y: y)] = Tile(char)
+func parseGrid<Tile>(from lines: [String], using builder: (Coordinate, Character) -> Tile?) -> [Coordinate: Tile] {
+    lines.enumerated().reduce(into: [:]) { result, pair in
+        let y = pair.offset
+        result = pair.element.enumerated().reduce(into: result) { result, pair in
+            let coord = Coordinate(x: pair.offset, y: y)
+            result[coord] = builder(coord, pair.element)
         }
     }
-    return map
+}
+
+func dfs<Node: Hashable>(start: Node, to end: Node, using getNextNodes: (Node) -> [(Node, Int)]) -> Int {
+    dfs(start: start, to: [end], using: getNextNodes).values.first!
+}
+
+func dfs<Node: Hashable>(start: Node, to ends: Set<Node>, using getNextNodes: (Node) -> [(Node, Int)]) -> [Node: Int] {
+    var maxDistances: [Node: Int] = [:]
+    var visited: Set<Node> = []
+
+    @discardableResult
+    func traverse(from node: Node, distance: Int) -> [Node: Int] {
+        if distance > 1 && ends.contains(node) {
+            let previous = maxDistances[node, default: 0]
+            maxDistances[node] = max(previous, distance)
+            return maxDistances
+        }
+        visited.insert(node)
+        for next in getNextNodes(node).filter({ visited.contains($0.0) == false }) {
+            traverse(from: next.0, distance: distance + next.1)
+        }
+        visited.remove(node)
+        return maxDistances
+    }
+
+    return traverse(from: start, distance: 0)
+}
+
+extension String {
+    func firstIndex(of char: Character) -> Int? {
+        self.firstIndex(of: char)?.utf16Offset(in: self)
+    }
 }
 
 enum Part1 {
     static func run(_ source: InputData) {
-        let map = loadTrailMap(source.lines)
-        let trailEntrance = Coordinate(x: 1, y: 0)
-        let trailExit = Coordinate(x: source.lines[0].count - 2, y: source.lines.count - 1)
+        let map = parseGrid(from: source.lines) { Tile($1) }
+        let trailEntrance = Coordinate(x: source.lines.first!.firstIndex(of: ".")!, y: 0)
+        let trailExit = Coordinate(x: source.lines.last!.firstIndex(of: ".")!, y: source.lines.count - 1)
 
-        func findAllPaths(from start: Coordinate, visited: Set<Coordinate> = []) -> [Set<Coordinate>] {
-            if start == trailExit {
-                return [visited]
-            }
-            let next = start.neighbours.filter {
-                guard visited.contains($0) == false else { return false }
-                return switch map[$0] {
+        let result = dfs(start: trailEntrance, to: trailExit) { coord in
+            coord.neighbours.filter {
+                switch map[$0] {
                 case .path: true
-                case .slope("<"): $0 == start.left
-                case .slope(">"): $0 == start.right
-                case .slope("^"): $0 == start.up
-                case .slope("v"): $0 == start.down
+                case .slope("<"): $0 == coord.left
+                case .slope(">"): $0 == coord.right
+                case .slope("^"): $0 == coord.up
+                case .slope("v"): $0 == coord.down
                 case .forest, .none: false
                 default: fatalError()
                 }
             }
-            let visited = visited.union([start])
-            return next.flatMap { findAllPaths(from: $0, visited: visited) }
+            .map { ($0, 1) }
         }
-
-        let result = findAllPaths(from: trailEntrance).map(\.count).max()!
 
         print("Part 1 (\(source)): \(result)")
     }
@@ -83,9 +108,9 @@ enum Part1 {
 
 enum Part2 {
     static func run(_ source: InputData) {
-        let map = loadTrailMap(source.lines)
-        let trailEntrance = Coordinate(x: 1, y: 0)
-        let trailExit = Coordinate(x: source.lines[0].count - 2, y: source.lines.count - 1)
+        let map = parseGrid(from: source.lines) { Tile($1) }
+        let trailEntrance = Coordinate(x: source.lines.first!.firstIndex(of: ".")!, y: 0)
+        let trailExit = Coordinate(x: source.lines.last!.firstIndex(of: ".")!, y: source.lines.count - 1)
 
         let junctions: [Coordinate] = map.reduce(into: []) { result, pair in
             guard pair.value != .forest else { return }
@@ -96,45 +121,14 @@ enum Part2 {
             }
         }
 
-        func findLongestDistances(from start: Coordinate, to ends: [Coordinate]) -> [(end: Coordinate, distance: Int)] {
-            func findAllDistances(from start: Coordinate, to ends: [Coordinate], path: Set<Coordinate> = []) -> [(end: Coordinate, distance: Int)] {
-                let path = path.union([start])
-                if ends.contains(start) && path.count > 1 {
-                    return [(start, path.count - 1)]
-                }
-                let next = start.neighbours.filter {
-                    guard path.contains($0) == false else { return false }
-                    return map[$0, default: .forest] != .forest
-                }
-                return next.flatMap { findAllDistances(from: $0, to: ends, path: path) }
+        let pathsBetweenJunctions = junctions.reduce(into: [:]) { result, junction in
+            result[junction] = dfs(start: junction, to: Set(junctions)) { coord in
+                coord.neighbours.filter { map[$0, default: .forest] != .forest }.map { ($0, 1) }
             }
-            return findAllDistances(from: start, to: ends).reduce(into: Dictionary<Coordinate, Int>()) { result, pair in
-                let previous = result[pair.end, default: 0]
-                result[pair.end] = max(previous, pair.distance)
-            }
-            .map { ($0.key, $0.value) }
+            .map { (end: $0.key, distance: $0.value) }
         }
 
-        let trails = junctions.reduce(into: [:]) { result, end in
-            result[end] = findLongestDistances(from: end, to: junctions)
-        }
-
-        func findLongestTrail(from start: Coordinate, path: [(junction: Coordinate, distance: Int)] = []) -> [(junction: Coordinate, distance: Int)] {
-            if start == trailExit {
-                return path
-            }
-            let visited = Set(path.map(\.junction) + [start])
-            let nextTrails = trails[start]!
-                .filter { visited.contains($0.end) == false }
-            return nextTrails.map {
-                let nextTrail = findLongestTrail(from: $0.end, path: path + [($0.end, $0.distance)])
-                return (trail: nextTrail, distance: nextTrail.reduce(0) { $0 + $1.distance })
-            }
-            .max(by: { $0.distance < $1.distance })?.trail ?? []
-        }
-
-        let longestPath = findLongestTrail(from: trailEntrance)
-
-        print("Part 2 (\(source)): \(longestPath.reduce(0) { $0 + $1.distance })")
+        let result = dfs(start: trailEntrance, to: trailExit, using: { pathsBetweenJunctions[$0]! })
+        print("Part 2 (\(source)): \(result)")
     }
 }
